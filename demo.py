b04a4cae650404930d2a61f4701d54891f06882a
Mwiza Simbeye, (Leo Kamali) <mwizasimbeye@gmail.com>
2017-05-29 11:53:19 +0200

    differented series method

--- demo.py
+++ demo.py
@@ -1,6 +1,7 @@
 # plot the timeseries data, Visualizing data is good.
 from pandas import Series
-import matplotlib.pyplot as plt
+import numpy as np
+from statsmodels.tsa.arima_model import ARIMA
 # load dataset
 series = Series.from_csv('data/daily-minimum-temperatures.csv', header=0)
 
@@ -10,3 +11,20 @@ dataset, validation = series[0:split_point], series[split_point:]
 print ('Dataset %d, Validation %d' % (len(dataset), len(validation)))
 dataset.to_csv('dataset.csv')
 validation.to_csv('validation.csv')
+
+# create differenced series
+def differenced(dataset, interval=1):
+    diff = list()
+    for i in range(interval, len(dataset)):
+        value = dataset[i] - dataset[i - interval]
+        diff.append(value)
+    return np.array(diff)
+
+# load dataset for ARIMA model
+series = Series.from_csv('dataset.csv', header=None)
+# seasonal difference
+
+# invert differenced value
+def inverve_difference(history, yhat, interval=1):
+    return yhat + history[-history]
+
